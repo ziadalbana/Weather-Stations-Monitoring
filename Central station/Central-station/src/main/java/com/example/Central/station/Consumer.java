13f3f42063a599bf9e91897475f04df2129b45b4
ZiadElabd <ziad.elabd91@gmail.com>
2023-05-22 17:18:40 +0300

    Partition parquet files by time and station ID

--- Central station/Central-station/src/main/java/com/example/Central/station/Consumer.java
+++ Central station/Central-station/src/main/java/com/example/Central/station/Consumer.java
@@ -4,14 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 public class Consumer {
 
-    List<StationData> records = new LinkedList<>();
-    Parquet parquet = new Parquet();
+    private final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd-HH");
+
+    private final Map<Integer, List<StationData>> stations_records = new HashMap<>();
+
+    int number_of_msg = 0;
+
+    Parquet parquet = new Parquet("./archive");
+    Bitcask bitcask = new Bitcask("./store");
 
    @KafkaListener(topics = "station",
            groupId = "springboot",
@@ -26,20 +32,62 @@ public class Consumer {
            ObjectMapper objectMapper = new ObjectMapper();
            StationData stationData = objectMapper.readValue(msg, StationData.class);
 
+           int station_id = stationData.getStationId();
 
-           System.out.println(stationData.getStationId());
-           System.out.println(stationData.getSNo());
-           System.out.println(stationData.getBatteryStatus());
-           System.out.println(stationData.getStatusTimestamp());
-           System.out.println(stationData.getWeather().getHumidity());
-           System.out.println(stationData.getWeather().getTemperature());
-           System.out.println(stationData.getWeather().getWindSpeed());
-
-           records.add(stationData);
-
-           if (records.size() == 10){
-               parquet.write("file.parquet", records);
+           if(station_id < 1 || station_id > 10){ // wrong station_id
+               return;
            }
+
+           bitcask.put(String.valueOf(stationData.getStationId()), msg);
+
+           if(stations_records.containsKey(station_id)){
+               stations_records.get(station_id).add(stationData);
+           }else{
+               stations_records.put(station_id, new LinkedList<>());
+               stations_records.get(station_id).add(stationData);
+           }
+
+           this.number_of_msg++;
+
+           if(this.number_of_msg < 10_000)
+               return;
+
+           // write to parquet
+           for (int i = 1; i <= 10; i++) {
+               if (!stations_records.containsKey(i)) continue;
+
+               List<StationData> records = stations_records.get(i);
+               stations_records.get(i).clear();
+
+               if (records.size() == 1) {
+                   parquet.write(getFileNameFromTimestamp(records.get(0).getStatusTimestamp()) + "_" + i, records);
+               }
+
+               records.sort(Comparator.comparingLong(StationData::getStatusTimestamp));
+
+               int l = 0, r = 0;
+
+               String start = getFileNameFromTimestamp(records.get(0).getStatusTimestamp());
+
+               while (r < records.size()) {
+                   String cur = getFileNameFromTimestamp(records.get(r).getStatusTimestamp());
+                   if (start.equals(cur)) {
+                       r++;
+                   } else {
+                       parquet.write(start + "_" + i, records.subList(l, r));
+                       l = r;
+                       start = getFileNameFromTimestamp(records.get(l).getStatusTimestamp());
+                       r++;
+                   }
+               }
+
+               if (l != records.size() - 1) {
+                   parquet.write(start + "_" + i, records.subList(l, r));
+               }
+           }
+
+
+
 
 
        } catch (Exception e) {
@@ -47,4 +95,10 @@ public class Consumer {
        }
 
    }
+
+    public String getFileNameFromTimestamp(long timestamp) {
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd-HH");
+        Date date = new Date(timestamp);
+        return dateFormatter.format(date);
+    }
 }
--- Central station/Central-station/src/main/java/com/example/Central/station/Parquet.java
+++ Central station/Central-station/src/main/java/com/example/Central/station/Parquet.java
@@ -4,9 +4,9 @@ package com.example.Central.station;
 
 import org.apache.avro.reflect.ReflectData;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetWriter;
-import org.apache.parquet.hadoop.ParquetFileWriter;
 import org.apache.parquet.hadoop.ParquetWriter;
 
 import java.io.IOException;
@@ -17,9 +17,27 @@ import org.apache.parquet.hadoop.ParquetFileWriter.Mode;
 import org.apache.parquet.hadoop.metadata.CompressionCodecName;
 public class Parquet {
 
+    private final String directory;
+
+    public Parquet(String dir){
+        directory = dir;
+        try {
+            Configuration conf = new Configuration();
+            FileSystem fileSystem = FileSystem.get(conf);
+
+            // Create the archive directory if it doesn't exist
+            Path archivePath = new Path("./archive");
+            if (!fileSystem.exists(archivePath)) {
+                fileSystem.mkdirs(archivePath);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public  void write(String fileName, List<StationData> records) {
 
-        try (ParquetWriter<StationData> writer = AvroParquetWriter.<StationData>builder(new Path(fileName))
+        try (ParquetWriter<StationData> writer = AvroParquetWriter.<StationData>builder(new Path(this.directory + fileName))
                 .withSchema(ReflectData.AllowNull.get().getSchema(StationData.class))
                 .withDataModel(ReflectData.get())
                 .withConf(new Configuration())
